0dec30fc5d244bf55281617b0416ee1baecd8baa
Kanthakumaran S <kanthakumar915@gmail.com>
2025-04-06 22:06:33 +0530

    SingleTonPattern

---
+++ SingletonPattern/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace SingletonPattern
+{
+    public class AppSettings
+    {
+        private readonly String dbConnectionString;
+        private static AppSettings instance;
+
+        public String DbConnectionString => dbConnectionString;
+
+        private AppSettings()
+        {
+            //read from a file and initialize here
+            dbConnectionString = "SomeThing";
+        }
+        public static AppSettings GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new AppSettings();
+            }
+            return instance;
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using SingletonPattern;
+//Private constructor to restrict multiple instantiation
+//Access Global resources with single instance throughout
+
+AppSettings appSettings = AppSettings.GetInstance();
+AppSettings appSettingsCopy = AppSettings.GetInstance();
+Console.WriteLine(appSettings.DbConnectionString);
+Console.WriteLine(appSettingsCopy.DbConnectionString);
+Console.WriteLine((appSettingsCopy == appSettings).ToString());
