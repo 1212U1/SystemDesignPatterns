d254901de8dfe7ec67491fe9cdab8fe4477bbb71
Kanthakumaran S <kanthakumar915@gmail.com>
2025-04-07 22:05:58 +0530

    BuilderPattern

---
+++ BuilderPattern/House.cs
@@ -0,0 +1,57 @@
+﻿namespace BuilderPattern
+{
+    public class House
+    {
+        private String name;
+        private String type;
+        private Boolean hasSwimmingPool;
+        private Boolean hasGarden;
+
+        private House(HouseBuilder houseBuilder)
+        {
+            this.Name = houseBuilder.name;
+            this.Type = houseBuilder.type;
+            this.HasSwimmingPool = houseBuilder.hasSwimmingPool;
+            this.HasGarden = houseBuilder.hasGarden;
+
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+        public bool HasSwimmingPool { get => hasSwimmingPool; set => hasSwimmingPool = value; }
+        public bool HasGarden { get => hasGarden; set => hasGarden = value; }
+
+        public String GetHouseDescription()
+        {
+            return String.Format("{0} {1} {2} {3}", this.Name, this.Type, this.HasSwimmingPool, this.HasGarden);
+        }
+
+        public class HouseBuilder
+        {
+            public String name;
+            public String type;
+            public Boolean hasSwimmingPool;
+            public Boolean hasGarden;
+
+            public HouseBuilder(String name, String type)
+            {
+                this.name = name;
+                this.type = type;
+            }
+            public HouseBuilder SetSwimmingPoolStatus(Boolean hasSwimmingPoolStatus)
+            {
+                this.hasSwimmingPool = hasSwimmingPoolStatus;
+                return this;
+            }
+            public HouseBuilder SetGardenStatus(Boolean hasGarden)
+            {
+                this.hasGarden = hasGarden;
+                return this;
+            }
+            public House Build()
+            {
+                return new House(this);
+            }
+        }
+    }
+}
---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using BuilderPattern;
+
+//Helps to create complex objects with method chaining and nested class Build method
+//Immutable after creation using private set property
+//The private constructor in the House class takes all the parameters.
+//It is only accessible from within the Builder class, ensuring that House objects are always created via the builder.
+House house = new House.HouseBuilder("KanthaHome", "Apartment").SetGardenStatus(true)
+                        .SetSwimmingPoolStatus(true).Build();
+Console.WriteLine(house.GetHouseDescription());
